8989c6eaec2afcb74de7b6256ff2bb0f37fc3fab
linesh <linesh.simpcity@gmail.com>
2017-03-12 18:22:34 +0800

    [Linesh] Refactor: Generate random enemy position in the row

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [
-	new Enemy(20, GameBoard.row(1), Speed.EXTREMELY_SLOW),
-	new Enemy(200, GameBoard.row(1), Speed.SLOW),
-	new Enemy(303, GameBoard.row(2), Speed.EXTREMELY_SLOW),
-	new Enemy(20, GameBoard.row(2), Speed.EXTREMELY_FAST),
-	new Enemy(200, GameBoard.row(3), Speed.NORMAL),
-	new Enemy(2, GameBoard.row(3), Speed.EXTREMELY_SLOW)
-];
+var randomPositionInRow = function () {
+	var boardWidth = GameBoard.boardWidth;
+	return Math.floor(Math.random() * boardWidth)
+};
+
 var player = new Player(GameBoard.column(3), GameBoard.row(4));
+var allEnemies = [
+	new Enemy(randomPositionInRow(), GameBoard.row(1), Speed.EXTREMELY_SLOW),
+	new Enemy(randomPositionInRow(), GameBoard.row(1), Speed.SLOW),
+	new Enemy(randomPositionInRow(), GameBoard.row(2), Speed.EXTREMELY_SLOW),
+	new Enemy(randomPositionInRow(), GameBoard.row(2), Speed.EXTREMELY_FAST),
+	new Enemy(randomPositionInRow(), GameBoard.row(3), Speed.NORMAL),
+	new Enemy(randomPositionInRow(), GameBoard.row(3), Speed.EXTREMELY_SLOW)
+];
