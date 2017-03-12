e17db7e0efd66d6c9004b939f797aa4b0c37e4fc
linesh <linesh.simpcity@gmail.com>
2017-03-12 21:18:45 +0800

    [Linesh] Chore: Adjust the top level enemy speed

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ var randomPositionInRow = function () {
 
 var player = new Player(GameBoard.column(3), GameBoard.row(4));
 var allEnemies = [
-	new Enemy(randomPositionInRow(), GameBoard.row(1), Speed.EXTREMELY_SLOW),
+	new Enemy(randomPositionInRow(), GameBoard.row(1), Speed.FAST),
+	new Enemy(randomPositionInRow(), GameBoard.row(1), Speed.VERY_FAST),
 	new Enemy(randomPositionInRow(), GameBoard.row(1), Speed.SLOW),
 	new Enemy(randomPositionInRow(), GameBoard.row(2), Speed.EXTREMELY_SLOW),
 	new Enemy(randomPositionInRow(), GameBoard.row(2), Speed.EXTREMELY_FAST),
