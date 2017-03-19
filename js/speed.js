f48c052ffe5252ace8af5fd07b476e7dc532d8b7
Linesh <linesh.simpcity@gmail.com>
2017-03-19 19:26:58 +0800

    [Linesh][#16] Feature: add differnet level of speeds for enemies

--- js/speed.js
+++ js/speed.js
@@ -1,5 +1,9 @@
 export const Speed = {
-   VERY_SLOW: 200,
-   NORMAL: 300,
-   VERY_FAST: 500
+   EXTREMELY_SLOW: 50,
+   VERY_SLOW: 150,
+   SLOW: 250,
+   NORMAL: 400,
+   FAST: 500,
+   VERY_FAST: 700,
+   EXTREMELY_FAST: 1000
 }
