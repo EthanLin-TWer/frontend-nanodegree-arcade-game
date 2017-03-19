734dc53803b45d4271aab19d74cd1e71882d508d
Linesh <linesh.simpcity@gmail.com>
2017-03-19 12:48:18 +0800

    [Linesh][#15] Style: fix eslint no-undef warning

--- test/unit/player.spec.js
+++ test/unit/player.spec.js
@@ -1,10 +1,11 @@
+import { describe, it } from 'mocha'
 import { expect } from 'chai'
 import { Player } from '../../js/Player'
 
 describe('game player', () => {
    it('player should be able to move left', () => {
-      let player = new Player(202, 83 * 3 + 55);
-      player.handleInput('left');
+      let player = new Player(202, 83 * 3 + 55)
+      player.handleInput('left')
 
       expect(player.x).to.equal(202)
       expect(player.y).to.equal(83 * 3 + 55)
