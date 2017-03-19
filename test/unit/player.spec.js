324440727f2cb962877897fc19612ae83b290abd
Linesh <linesh.simpcity@gmail.com>
2017-03-19 12:29:46 +0800

    [Linesh][#15] Style: rename and format player.spec.js

---
+++ test/unit/player.spec.js
@@ -0,0 +1,7 @@
+import { expect } from 'chai'
+
+describe('', () => {
+   it('', () => {
+      expect(1).to.equal(1)
+   })
+})
--- test/unit/sample.spec.js
+++
@@ -1,7 +0,0 @@
-import {expect} from 'chai'
-
-describe('', ()=>{
-   it('',()=>{
-      expect(1).to.equal(1)
-   })
-})
