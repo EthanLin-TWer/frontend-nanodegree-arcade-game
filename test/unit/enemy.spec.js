74274e6d2f4d324b3738dfb2976eb29e96d95ba0
Linesh <linesh.simpcity@gmail.com>
2017-03-19 23:15:02 +0800

    [Linesh][#22] Test: group Enemy.spec.js tests

--- test/unit/enemy.spec.js
+++ test/unit/enemy.spec.js
@@ -4,65 +4,71 @@ import { Enemy } from '../../js/Enemy'
 import { Game, Speed } from '../../js/constants'
 import { Player } from '../../js/Player'
 
-describe('game enemy', () => {
-   it('should be able to move', () => {
-      let enemy = new Enemy(Game.col(1), Game.row(2), Speed.NORMAL)
-      enemy.update(1)
+describe('enemy', () => {
 
-      expect(enemy.x).to.not.equal(Game.col(1))
+   describe('move functionality', () => {
+      it('should be able to move', () => {
+         let enemy = new Enemy(Game.col(1), Game.row(2), Speed.NORMAL)
+         enemy.update(1)
+
+         expect(enemy.x).to.not.equal(Game.col(1))
+      })
+
+      it('should be able to move very fast', () => {
+         let enemy = new Enemy(Game.col(1), Game.row(2), Speed.VERY_FAST)
+         enemy.update(1)
+
+         expect(enemy.speed).to.equal(Speed.VERY_FAST)
+      })
+
+      it('should be able to move very slow', () => {
+         let enemy = new Enemy(Game.col(1), Game.row(2), Speed.VERY_SLOW)
+         enemy.update(1)
+
+         expect(enemy.speed).to.equal(Speed.VERY_SLOW)
+      })
    })
 
-   it('should be able to move very fast', () => {
-      let enemy = new Enemy(Game.col(1), Game.row(2), Speed.VERY_FAST)
-      enemy.update(1)
+   describe('collision detection', () => {
+      it('should not collide when player is not in the same row', () => {
+         let enemy = new Enemy(Game.col(1), Game.row(2), Speed.FAST)
+         let player = new Player(Game.col(1), Game.row(3))
 
-      expect(enemy.speed).to.equal(Speed.VERY_FAST)
+         let collides = enemy.checkCollision(player)
+         expect(collides).to.be.false
+      })
+
+      it('should not collide when player isn\'t close enough', () => {
+         let enemy = new Enemy(Game.col(2), Game.row(2), Speed.FAST)
+         let player = new Player(Game.col(1), Game.row(2))
+
+         let collides = enemy.checkCollision(player)
+         expect(collides).to.be.false
+      })
+
+      it('should collide when player meets enemy in the same position', () => {
+         let enemy = new Enemy(Game.col(2), Game.row(2), Speed.FAST)
+         let player = new Player(Game.col(2), Game.row(2))
+
+         let collides = enemy.checkCollision(player)
+         expect(collides).to.be.true
+      })
+
+      it('should collide when player is less than 12px closer to the enemy', () => {
+         let enemy = new Enemy(200, Game.row(3), Speed.FAST)
+         let player = new Player(212, Game.row(3))
+
+         let collides = enemy.checkCollision(player)
+         expect(collides).to.be.true
+      })
+
+      it('should collide when player is more than 13px far away from the enemy', () => {
+         let enemy = new Enemy(187, Game.row(3), Speed.FAST)
+         let player = new Player(200, Game.row(3))
+
+         let collides = enemy.checkCollision(player)
+         expect(collides).to.be.false
+      })
    })
 
-   it('should be able to move very slow', () => {
-      let enemy = new Enemy(Game.col(1), Game.row(2), Speed.VERY_SLOW)
-      enemy.update(1)
-
-      expect(enemy.speed).to.equal(Speed.VERY_SLOW)
-   })
-
-   it('should not collide when player is not in the same row', () => {
-      let enemy = new Enemy(Game.col(1), Game.row(2), Speed.FAST)
-      let player = new Player(Game.col(1), Game.row(3))
-
-      let collides = enemy.checkCollision(player)
-      expect(collides).to.be.false
-   })
-
-   it('should not collide when player isn\'t close enough', () => {
-      let enemy = new Enemy(Game.col(2), Game.row(2), Speed.FAST)
-      let player = new Player(Game.col(1), Game.row(2))
-
-      let collides = enemy.checkCollision(player)
-      expect(collides).to.be.false
-   })
-
-   it('should collide when player meets enemy in the same position', () => {
-      let enemy = new Enemy(Game.col(2), Game.row(2), Speed.FAST)
-      let player = new Player(Game.col(2), Game.row(2))
-
-      let collides = enemy.checkCollision(player)
-      expect(collides).to.be.true
-   })
-
-   it('should collide when player is less than 12px closer to the enemy', () => {
-      let enemy = new Enemy(200, Game.row(3), Speed.FAST)
-      let player = new Player(212, Game.row(3))
-
-      let collides = enemy.checkCollision(player)
-      expect(collides).to.be.true
-   })
-
-   it('should collide when player is more than 13px far away from the enemy', () => {
-      let enemy = new Enemy(187, Game.row(3), Speed.FAST)
-      let player = new Player(200, Game.row(3))
-
-      let collides = enemy.checkCollision(player)
-      expect(collides).to.be.false
-   })
 })
