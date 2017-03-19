00489b15c997c1cb24da75a028852e2ebe53a393
Linesh <linesh.simpcity@gmail.com>
2017-03-19 19:09:57 +0800

    [Linesh][#16] Feature: refactor enemy to es6 class

--- js/app.js
+++ js/app.js
@@ -1,26 +1,20 @@
 import Resources from './resources'
 
 // Enemies our player must avoid
-var Enemy = function () {
-   // Variables applied to each of our instances go here,
-   // we've provided one for you to get started
+export class Enemy {
+   constructor(x, y) {
+      this.x = x
+      this.y = y
+      this.sprite = 'images/enemy-bug.png'
+   }
 
-   // The image/sprite for our enemies, this uses
-   // a helper we've provided to easily load images
-   this.sprite = 'images/enemy-bug.png';
-};
+   update(dt) {
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function (dt) {
-   // You should multiply any movement by the dt parameter
-   // which will ensure the game runs at the same speed for
-   // all computers.
-};
+   }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function () {
-   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+   render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
+   }
+}
 
-export const allEnemies = [new Enemy()]
+export const allEnemies = [new Enemy(1, 2)]
