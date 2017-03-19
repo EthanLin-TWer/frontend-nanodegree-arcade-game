72e352f2c91fee858bb62da4cdd71bd9e235d1ae
Linesh <linesh.simpcity@gmail.com>
2017-03-19 19:19:18 +0800

    [Linesh][#16] Feature: enemy should be able to move very fast

--- test/unit/enemy.spec.js
+++ test/unit/enemy.spec.js
@@ -10,4 +10,12 @@ describe('game enemy', () => {
 
       expect(enemy.x).to.not.equal(1)
    })
+
+   it('should be able to move very fast', () => {
+      let enemy = new Enemy(1, 2, 500)
+      enemy.update(1/1000)
+
+      expect(enemy.speed).to.equal(500)
+      expect(enemy.x).to.equal(1 + 1 / 1000 * 500)
+   })
 })
