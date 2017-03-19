e8dca9b709edaeb4808c7aaf74190da557f02c7d
Linesh <linesh.simpcity@gmail.com>
2017-03-19 23:32:42 +0800

    [Linesh][#22] Refactor: extract object center 55 as variables

--- js/constants/game.js
+++ js/constants/game.js
@@ -1,12 +1,17 @@
 export class Game {
    static CELL_WIDTH = 101
    static CELL_HEIGHT = 83
+   static OBJECT_CENTER = 55
+
    static COLLISION_RADIUS = 12
+
    static HORIZON_CELLS = 5
+   static VERTICAL_CELLS = 6
+
    static BOARD_WIDTH = Game.CELL_WIDTH * Game.HORIZON_CELLS
 
    static row(y) {
-      return this.CELL_HEIGHT * (y - 1) + 55
+      return this.CELL_HEIGHT * (y - 1) + this.OBJECT_CENTER
    }
    static col(x) {
       return this.CELL_WIDTH * (x - 1)
