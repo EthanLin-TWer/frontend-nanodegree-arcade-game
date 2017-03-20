dd6bc17562889ee903f791104bd4244e558ca35f
Linesh <linesh.simpcity@gmail.com>
2017-03-20 08:08:55 +0800

    [Linesh][#25] Style: add quote-props rule

--- js/engine.js
+++ js/engine.js
@@ -197,10 +197,10 @@ const Engine = (function () {
    // Player.handleInput() method. You don't need to modify this.
    document.addEventListener('keyup', function (e) {
       const allowedKeys = {
-         37: 'left',
-         38: 'up',
-         39: 'right',
-         40: 'down'
+         '37': 'left',
+         '38': 'up',
+         '39': 'right',
+         '40': 'down'
       }
 
       player.handleInput(allowedKeys[e.keyCode])
