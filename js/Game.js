b7bdbc57e1748b7eee82c5137fcaf1e5363a58be
Linesh <linesh.simpcity@gmail.com>
2017-03-25 12:42:33 +0800

    [Linesh][#15] Refactor: refactor Game.col(x) method

--- js/Game.js
+++ js/Game.js
@@ -1,6 +1,6 @@
 export class Game {
    static CELL_WIDTH = 101
    static col(x) {
-      return 202
+      return Game.CELL_WIDTH * (x - 1)
    }
 }
--- test/player.spec.js
+++ test/player.spec.js
@@ -5,7 +5,7 @@ import { expect } from 'chai'
 describe('player', () => {
    describe('move functionalities', () => {
       it('x should be 0 when move left given current x is 101', () => {
-         const player = new Player(101, 0)
+         const player = new Player(Game.col(2), 0)
          const position = player.handleInput('left')
 
          expect(position.x).to.equal(101 - Game.CELL_WIDTH)
